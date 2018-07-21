714176e8a0f044a0cd620079916410ad893b8109
19970120dty <964239668@qq.com>
2018-07-21 19:09:23 +0800

    初步修改成boot实现

---
+++ dts-server/src/main/java/io/github/jasonnull/dts/server/conf/DataSourceConfig.java
@@ -0,0 +1,25 @@
+package io.github.jasonnull.dts.server.conf;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by dty on 2018/7/21.
+ */
+@Configuration
+public class DataSourceConfig {
+    @Bean(name = "dataSource")
+    @Qualifier(value = "dataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "c3p0")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().type(ComboPooledDataSource.class).build();
+    }
+}
---
+++ dts-server/src/main/java/io/github/jasonnull/dts/server/conf/SchedulerConfig.java
@@ -0,0 +1,41 @@
+package io.github.jasonnull.dts.server.conf;
+
+import io.github.jasonnull.dts.server.schedule.JobDynamicScheduler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by dty on 2018/7/21.
+ */
+@Configuration
+public class SchedulerConfig {
+    @Value("${dts.job.accessToken}")
+    private String accessToken;
+
+    @Bean("schedulerFactory")
+    public SchedulerFactoryBean schedulerFactory(DataSource dataSource) {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        Resource resource = new ClassPathResource("quartz.properties");
+        schedulerFactoryBean.setConfigLocation(resource);
+        schedulerFactoryBean.setDataSource(dataSource);
+        schedulerFactoryBean.setAutoStartup(true);
+        schedulerFactoryBean.setStartupDelay(10);
+        schedulerFactoryBean.setOverwriteExistingJobs(true);
+        schedulerFactoryBean.setApplicationContextSchedulerContextKey("applicationContextKey");
+        return schedulerFactoryBean;
+    }
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    public JobDynamicScheduler xxlJobDynamicScheduler(SchedulerFactoryBean schedulerFactory) {
+        JobDynamicScheduler jobDynamicScheduler = new JobDynamicScheduler();
+        jobDynamicScheduler.setAccessToken(accessToken);
+        jobDynamicScheduler.setScheduler(schedulerFactory.getObject());
+        return jobDynamicScheduler;
+    }
+}
